c27de0d16a70c4d96ff6f2955c3223ac62444abd
rsentgerath <31182848+rsentgerath@users.noreply.github.com>
2017-08-20 22:34:30 -0400

    Create go-package-consumer.go

---
+++ go-package-consumer.go
@@ -0,0 +1,18 @@
+package main
+
+import (
+  "os"
+  "github.com.olekukonko/tablewriter"
+)
+
+func main () {
+  data := [][]string{
+    []string{"Alfred", "15", "10/20", "(10.32, 11.5, 4.99)"},
+    []string{"Peter Smith", "123", "11/55", "(1234, 44, 55555)"},
+  }
+  
+  table := tablewriter.NewWriter(os.Stdout)
+  table.SetHeader([]string{"NPC", "Speed", "Power", "Location"})
+  table.AppendBulk(data)
+  table.Render()
+}
